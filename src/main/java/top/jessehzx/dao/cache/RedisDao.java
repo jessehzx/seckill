49d068220d6e810d94355f2ebe3e03a829990fd9
jessehzx <jessehuangzx@gmail.com>
2018-05-15 23:07:32 +0800

    add method

--- src/main/java/top/jessehzx/dao/cache/RedisDao.java
+++ src/main/java/top/jessehzx/dao/cache/RedisDao.java
@@ -1,8 +1,27 @@
 package top.jessehzx.dao.cache;
 
+import top.jessehzx.entity.Seckill;
+
 /**
  * @author jessehzx
  * @date 2018/5/15
  */
 public class RedisDao {
+
+    private final JedisPool jedisPool;
+
+    public RedisDao(String ip, int port) {
+        jedisPool = new JedisPool(ip, port);
+    }
+
+    public Seckill getSeckill(long seckillId) {
+
+        return null;
+    }
+
+    public Seckill putSeckill(Seckill seckill) {
+
+        return null;
+    }
+
 }
