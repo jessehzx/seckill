4406882f844b089546b3013f63a9241573309c2b
jessehzx <jessehuangzx@gmail.com>
2018-06-07 11:51:13 +0800

    update code

--- src/main/java/top/jessehzx/dao/cache/RedisDao.java
+++ src/main/java/top/jessehzx/dao/cache/RedisDao.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import top.jessehzx.dto.Exposer;
 import top.jessehzx.entity.Seckill;
 
 /**
